e03f57c2b10d8f5bb69b49a7edcfa0f5ddd85aea
sophianexus <sophianexusclient@gmail.com>
2026-03-21 03:47:29 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Headline from './components/Headline/Headline.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Headline />
   </StrictMode>,
 )
