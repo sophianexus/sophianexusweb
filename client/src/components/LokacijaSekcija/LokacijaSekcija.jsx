d1c39b30fdfdb4f2098bc58b3d9338bc2e0d26a1
sophianexus <sophianexusclient@gmail.com>
2026-04-02 23:54:27 +0200

    Update LokacijaSekcija.jsx

--- client/src/components/LokacijaSekcija/LokacijaSekcija.jsx
+++ client/src/components/LokacijaSekcija/LokacijaSekcija.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { useInView } from "react-intersection-observer";
 import "./LokacijaSekcija.css";
-import slika1 from "../../assets/stacesdobiti.jpg"; 
-import slika2 from "../../assets/rutina2.jpg";
-import slika3 from "../../assets/vis.jpg";
 
 function LokacijaSekcija() {
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
@@ -23,7 +20,7 @@ function LokacijaSekcija() {
           <div className="map-minimal">
             <iframe
               title="Lokacija Brezna"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2917.447543883017!2d18.9103857!3d43.0110196!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x134d1b115e3f3ede%3A0xc7c7fdb31d238a3c!2sEtno%20selo%20Montenegro!5e0!3m2!1ssr!2sme!4v1700000000000!5m2!1ssr!2sme"
+              src="https://maps.google.com/maps?q=Etno%20selo%20Montenegro%20Brezna&t=k&z=15&ie=UTF8&iwloc=&output=embed"
               width="100%"
               height="100%"
               style={{ border: 0 }}
@@ -35,13 +32,13 @@ function LokacijaSekcija() {
         <div className="loc-clean-gallery">
           <div className="gallery-layout">
             <div className="img-box small-top">
-              <img src={slika1} alt="Brezna 1" />
+              <img src="/images/stacesdobiti.jpg" alt="Image not found." />
             </div>
             <div className="img-box medium-center">
-              <img src={slika3} alt="Brezna 2" />
+              <img src="/images/vis.jpg" alt="Image not found." />
             </div>
             <div className="img-box small-bottom">
-              <img src={slika2} alt="Brezna 3" />
+              <img src="/images/rutina2.jpg" alt="Image not found." />
             </div>
           </div>
         </div>
